04ac08a526986762c1fb97557d1a898d978c20a4
Michael Cortez-Mejia <bayarea6fifty@gmail.com>
2022-05-17 04:01:01 -0700

    automated optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const path = require('path');
 const webpack = require('webpack');
 
@@ -7,13 +8,45 @@ const webpack = require('webpack');
 module.exports = {
     // entry property; the root of the bundle and the beginning of the dependency graph
     // relative path to the client's code 
-    entry: './assets/js/script.js',
+    entry:  {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
     // webpack takes the entry point, bundles that code, and outputs to a specified folder
     // best practice to put your bundled code into a folder named dist
     output: {
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+
+    },
+
+
     // set the mode you want webpack to run on. Default is 'production' mode, 
     // where webpack will minify code automatically. 
     plugins: [
@@ -21,6 +54,10 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static" // the report outputs to an HTML file in dist folder. 
+            //Can also set "disable" to temporarily stop auto reporting in browser
+        })
     ],
     mode: 'development'
 };
